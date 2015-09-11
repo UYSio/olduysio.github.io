694ead87c05af85941019cb9d39afb84acce527b
Juan Matthys Uys <opyate@gmail.com>
2015-09-11 11:54:18 +0100

    set colours for tags

--- js/main.js
+++ js/main.js
@@ -19,11 +19,22 @@ window.onload = function(){
     };
   }
 
+  var wordToNumber = function(word) {
+    var number = 0;
+    for (var i=0; i<word.length; i++) {
+      number += word[i].charCodeAt(0);
+    }
+    //console.log(word, number);
+    number = parseFloat('0.' + number);
+    //console.log(word, number);
+    return number; 
+  };
+
   var colors = ["#48466D", "#D4CEB0", "#FEAA2B", "#FF5757", "#6B3278", "#362999", "#4262C5", "#F6538F", "#BA6C65"];
   var tags = document.getElementsByClassName("post-tag");
-
   for (var i=0; i<tags.length; i++) {
-    var selectedColour = colors[Math.floor(Math.random()*colors.length)];
+    var tag = tags[i].className.split(' ')[1];
+    var selectedColour = colors[Math.floor(wordToNumber(tag)*colors.length)];
     tags[i].style.backgroundColor = selectedColour;
   }
 };
